1aca6a255d0aeb7c51d957f2009ba5a1a647ee3c
Dariusz Luksza <dariusz@luksza.org>
2013-05-08 20:04:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Extend Gerrit Web UI from JavaScript plugin
+=======
+
+To be able to use this plugin in gerrit you need to have [change I0876d83](https://gerrit-review.googlesource.com/45518) in yours Gerrit.
---
+++ src/main/js/hello-js-plugins.js
@@ -0,0 +1,12 @@
+var position = 2;
+var title = "Custome plugin row";
+var callback = function(changeNo) {
+  var widget = document.createElement("button");
+  widget.innerHTML = "click me!";
+  widget.onclick = function() {
+	  alert("Welcome in change: " + changeNo);
+  };
+  return widget;
+}
+
+gerrit.onChangeScreen().addChangeInfoRow(position, title, callback);
